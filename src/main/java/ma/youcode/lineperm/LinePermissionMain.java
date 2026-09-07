4876e619f135fa417dfd980298dfea8924d33aa6
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-07 17:04:07 +0100

    feat: implement ConsoleApp initialization and user input handling in LinePermissionMain

--- src/main/java/ma/youcode/lineperm/LinePermissionMain.java
+++ src/main/java/ma/youcode/lineperm/LinePermissionMain.java
@@ -1,6 +1,16 @@
 package ma.youcode.lineperm;
 
-public class LinePermissionMain {
+import ma.youcode.lineperm.ui.ConsoleApp;
 
-    
+public class LinePermissionMain {
+    private final ConsoleApp consoleApp;
+
+    LinePermissionMain(ConsoleApp consoleApp) {
+        this.consoleApp = consoleApp;
+    }
+
+    public static void main(String[] args) {
+        ConsoleApp consoleApp = new ConsoleApp();
+        new LinePermissionMain(consoleApp);
+    }
 }
--- src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
+++ src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
@@ -1,5 +1,22 @@
 package ma.youcode.lineperm.ui;
 
+import java.util.Scanner;
+
 public class ConsoleApp {
+    String choix;
+
+    Scanner scanner = new Scanner(System.in);
     
+    public ConsoleApp() {
+        start();
+    }
+
+    public void start() {
+        System.out.println("====================== LinePerm ====================");
+
+        System.out.println("Non Connecte ? Commandes : singup | login | help | exit\n");
+
+        System.out.print("lineperm> ");
+        choix = scanner.nextLine();
+    }
 }
