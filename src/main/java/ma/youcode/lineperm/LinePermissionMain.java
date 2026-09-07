152c754bcae923704e1c3dd2b2871f539e54311e
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-07 17:19:29 +0100

    feat: enhance LinePermissionMain with user signup handling and refactor ConsoleApp for public access

--- src/main/java/ma/youcode/lineperm/LinePermissionMain.java
+++ src/main/java/ma/youcode/lineperm/LinePermissionMain.java
@@ -1,16 +1,40 @@
 package ma.youcode.lineperm;
 
+import ma.youcode.lineperm.services.AuthService;
+import ma.youcode.lineperm.services.UserService;
 import ma.youcode.lineperm.ui.ConsoleApp;
 
 public class LinePermissionMain {
     private final ConsoleApp consoleApp;
+    private final AuthService authService;
+    private final UserService userService;
 
-    LinePermissionMain(ConsoleApp consoleApp) {
+    LinePermissionMain(ConsoleApp consoleApp , AuthService authService , UserService userService) {
         this.consoleApp = consoleApp;
+        this.authService = authService;
+        this.userService = userService;
     }
 
     public static void main(String[] args) {
         ConsoleApp consoleApp = new ConsoleApp();
-        new LinePermissionMain(consoleApp);
+        AuthService authService = new AuthService();
+        UserService userService = new UserService();
+
+        new LinePermissionMain(consoleApp , authService , userService);
+
+        switch (consoleApp.choix) {
+            case "signup":
+                String[] informations = consoleApp.singUpChoix();
+
+                String username = informations[0];
+                String password = informations[1];
+
+                authService.singUp(username, password);
+                break;
+            
+            default:
+                System.out.println("Choix don't existe.");
+                break;
+        }
     }
 }
--- src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
+++ src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
@@ -3,7 +3,7 @@ package ma.youcode.lineperm.ui;
 import java.util.Scanner;
 
 public class ConsoleApp {
-    String choix;
+    public String choix;
 
     Scanner scanner = new Scanner(System.in);
     
@@ -14,9 +14,21 @@ public class ConsoleApp {
     public void start() {
         System.out.println("====================== LinePerm ====================");
 
-        System.out.println("Non Connecte ? Commandes : singup | login | help | exit\n");
+        System.out.println("Non Connecte ? Commandes : signup | login | help | exit\n");
 
         System.out.print("lineperm> ");
         choix = scanner.nextLine();
+    }
+
+    public String[] singUpChoix() {
+        System.out.println("Entrer votre information.");
+
+        System.out.print("username : ");
+        String username = scanner.nextLine();
+
+        System.out.print("password : ");
+        String password = scanner.nextLine();
+
+        return new String[] {username , password};
     }
 }
