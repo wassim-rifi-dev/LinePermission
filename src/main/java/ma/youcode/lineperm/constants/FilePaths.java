2455b60812c308f78d1aa7bd06b74b3281520838
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-09 12:10:06 +0100

    feat: add mainFiles path to FilePaths class for file management

--- src/main/java/ma/youcode/lineperm/constants/FilePaths.java
+++ src/main/java/ma/youcode/lineperm/constants/FilePaths.java
@@ -2,4 +2,6 @@ package ma.youcode.lineperm.constants;
 
 public class FilePaths {
     public static String userFile = "src/main/resources/data/users.txt";
+
+    public static String mainFiles = "files/";
 }
