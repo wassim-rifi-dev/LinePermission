a360447fb30166f4ae2c8c049857e35438de78f0
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-23 11:18:56 +0000

    feat: implement FichierDAO class for Fichier data access operations

---
+++ src/main/java/ma/youcode/lineperm/dao/modelsDAO/FichierDAO.java
@@ -0,0 +1,21 @@
+package ma.youcode.lineperm.dao.modelsDAO;
+
+import ma.youcode.lineperm.dao.AbstractDAO;
+import ma.youcode.lineperm.models.Fichier;
+
+public class FichierDAO extends AbstractDAO<Fichier> {
+    @Override
+    public void save(Fichier fichier) {
+
+    }
+
+    @Override
+    public Fichier findById(long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Fichier fichier) {
+        
+    }
+}
