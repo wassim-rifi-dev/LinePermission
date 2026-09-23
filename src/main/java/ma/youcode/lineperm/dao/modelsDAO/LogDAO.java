f8cc66e17ba4bf28cd463aafab1108c9b339604b
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-23 12:06:38 +0000

    feat: add LogDAO class for Log data access operations

---
+++ src/main/java/ma/youcode/lineperm/dao/modelsDAO/LogDAO.java
@@ -0,0 +1,21 @@
+package ma.youcode.lineperm.dao.modelsDAO;
+
+import ma.youcode.lineperm.dao.AbstractDAO;
+import ma.youcode.lineperm.models.Log;
+
+public class LogDAO extends AbstractDAO<Log> {
+    @Override
+    public void save(Log log) {
+
+    }
+
+    @Override
+    public Log findById(long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Log log) {
+        
+    }
+}
