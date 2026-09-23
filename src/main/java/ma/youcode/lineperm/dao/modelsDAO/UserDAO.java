44640f3c67d7b410e4040422858f55ec3d58de91
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-23 11:17:32 +0000

    feat: add UserDAO class for user data access operations

---
+++ src/main/java/ma/youcode/lineperm/dao/modelsDAO/UserDAO.java
@@ -0,0 +1,21 @@
+package ma.youcode.lineperm.dao.modelsDAO;
+
+import ma.youcode.lineperm.dao.AbstractDAO;
+import ma.youcode.lineperm.models.User;
+
+public class UserDAO extends AbstractDAO<User> {
+    @Override
+    public void save(User user) {
+        
+    }
+
+    @Override
+    public User findById(long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+}
