01335bf3dc88efb3fc9f3e7c62447d09e795a317
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-24 08:57:01 +0000

    feat: implement save method in UserDAO for user data insertion

--- src/main/java/ma/youcode/lineperm/dao/modelsDAO/UserDAO.java
+++ src/main/java/ma/youcode/lineperm/dao/modelsDAO/UserDAO.java
@@ -1,12 +1,28 @@
 package ma.youcode.lineperm.dao.modelsDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import ma.youcode.lineperm.dao.AbstractDAO;
 import ma.youcode.lineperm.models.User;
 
 public class UserDAO extends AbstractDAO<User> {
     @Override
     public void save(User user) {
-        
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+        try (
+            Connection connection = AbstractDAO.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
     }
 
     @Override
